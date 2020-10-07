c144d56ab6d6244e09cb3cd0940dd37862cd13dc
Rana Alhmoud <ranaalhmoud92@gmail.com>
2020-10-07 13:13:39 +0300

    start working in the second page

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("connect")
+console.log("connect");
+
+
+$( document ).ready(function() {
+  $('.clickpro').click(function(){
+   window.open("project.html", "_blank")
+});
+  
+  $("#imgcompany").click(function(){
+  	window.open("./mywork/Company Website/company.html", "_blank")
+  })
+
+  $("#imguni").click(function(){
+  	window.open("./mywork/University/university.html", "_blank")
+  })
+});
